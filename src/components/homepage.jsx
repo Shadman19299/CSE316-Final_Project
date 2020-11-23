6c7dfb7b4a155f45ea29530054bbe85294dcc203
Jahin Imtiaz <jahin.imtiaz@stonybrook.edu>
2020-11-22 20:18:51 -0500

    Added Routes for each components

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import EmployeeHome from './employeeHomePage';
+import LabHome from './labHome';
+import LoginPage from './loginPage';
+import TestCollectionPage from './testCollectionPage';
+import PoolMappingPage from './poolMappingPage';
+import WellTestingPage from './wellTestingPage';
+import EmployeeLoginPage from './employeeLoginPage';
 
 class HompePage extends Component {
     state = {  }
@@ -9,10 +16,13 @@ class HompePage extends Component {
                 <div>
                     HOME PAGE
                 </div>
-                <Route></Route>
-                <Route></Route>
-                <Route></Route>
-                <Route></Route>
+                <Route path="/loginPage" component={LoginPage}></Route>
+                <Route path="/labHome" component={LabHome}></Route>
+                <Route path='/testCollectionPage' component={TestCollectionPage}></Route>
+                <Route path='/poolMappingPage' component={PoolMappingPage}></Route>
+                <Route path="./wellTestingPage" component={WellTestingPage}></Route>
+                <Route path="./employeeLoginPage" component={EmployeeLoginPage}></Route>
+                <Route path="/employeeHome/:employeeID" component={EmployeeHome}></Route>
             </Router>
          );
     }
